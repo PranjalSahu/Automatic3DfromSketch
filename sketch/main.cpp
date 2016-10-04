67ae74f00ab40461cc1893450da52aa050917aa0
Pranjal Sahu <pranjalsahu5@gmail.com>
2016-10-04 00:26:48 -0400

    blank commit

---
+++ sketch/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  sketch
+//
+//  Created by Pranjal Sahu on 10/4/16.
+//  Copyright © 2016 Pranjal Sahu. All rights reserved.
+//
+
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/glut.h>
+#include <GL/glu.h>
+#include <GL/gl.h>
+#endif
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <math.h>
+#include <queue>
+#include <list>
+#include <vector>
+#include <fstream>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
