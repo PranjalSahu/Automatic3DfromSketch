cb96da25706919af9c5d55a5a4870455c245bde8
Pranjal Sahu <pranjalsahu5@gmail.com>
2016-10-04 01:46:11 -0400

    triangle

--- sketch/main.cpp
+++ sketch/main.cpp
@@ -18,6 +18,7 @@
 #include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+
 #else
 #include <GL/glut.h>
 #include <GL/glu.h>
@@ -37,8 +38,39 @@
 
 
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+void renderScene(void) {
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-0.5,-0.5,0.0);
+    glVertex3f(0.5,0.0,0.0);
+    glVertex3f(0.0,0.5,0.0);
+    glEnd();
+    
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv) {
+    
+    
+
+    // init GLUT and create Window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(320,320);
+    glutCreateWindow("Lighthouse3D - GLUT Tutorial");
+    
+    // register callbacks
+    glutDisplayFunc(renderScene);
+    
+    
+    // get opengl version
+    // printf("%s", glGetString(GL_VERSION));
+
+    // enter GLUT event processing cycle
+    glutMainLoop();
+    
+    return 1;
 }
